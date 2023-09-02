f2ac4d6c164ca1deb10c98fb836522212780a7ec
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-02 13:24:59 +0600

    Finally Find a design idea for the circular progress indicator

--- app/src/main/java/com/playmakers/lifemetrics/MainActivity.kt
+++ app/src/main/java/com/playmakers/lifemetrics/MainActivity.kt
@@ -3,13 +3,20 @@ package com.playmakers.lifemetrics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.playmakers.lifemetrics.ui.theme.LifeMetricsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +29,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    // Greeting("Android")
+                    ProgressBar()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LifeMetricsTheme {
-        Greeting("Android")
+fun ProgressBar(){
+    Surface(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        CircularProgressIndicator(
+            progress = 1f,
+            strokeWidth = 10.dp,
+            color = Color.Gray
+        )
+
+        CircularProgressIndicator(
+            progress = 0.4f,
+            strokeWidth = 10.dp,
+        )
     }
 }
