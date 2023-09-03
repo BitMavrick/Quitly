621d3998a2d48b615bfc43c9388257ea46d2c64c
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-03 12:00:00 +0600

    Configuring the progress bar

--- app/src/main/java/com/playmakers/lifemetrics/ui/composables/HomeComposables.kt
+++ app/src/main/java/com/playmakers/lifemetrics/ui/composables/HomeComposables.kt
@@ -1,2 +1,50 @@
 package com.playmakers.lifemetrics.ui.composables
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.playmakers.lifemetrics.ui.theme.LifeMetricsTheme
+
+@Composable
+fun ProgressBar(){
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .aspectRatio(1f)
+    ) {
+        Surface(
+            Modifier.fillMaxSize()
+        ) {
+            CircularProgressIndicator(
+                progress = 1f,
+                strokeWidth = 10.dp,
+                color = Color.LightGray
+            )
+            CircularProgressIndicator(
+                progress = 0.4f,
+                strokeWidth = 10.dp,
+            )
+        }
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun ProgressBarPreview(){
+    LifeMetricsTheme {
+        ProgressBar()
+    }
+}
+
