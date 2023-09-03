42ef91ff65a9d12fa308daa5a447b3bbe4d2f366
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-03 13:36:50 +0600

    TopBar Done

--- app/src/main/java/com/playmakers/lifemetrics/ui/composables/HomeComposables.kt
+++ app/src/main/java/com/playmakers/lifemetrics/ui/composables/HomeComposables.kt
@@ -1,27 +1,55 @@
 package com.playmakers.lifemetrics.ui.composables
 
 import android.content.res.Configuration
-import android.graphics.Paint.Align
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.playmakers.lifemetrics.ui.theme.LifeMetricsTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(){
+    TopAppBar(
+        title = {
+            Text(
+                "LifeMetrics",
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        actions = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(
+                    imageVector = Icons.Filled.MoreVert,
+                    contentDescription = "Localized description"
+                )
+            }
+        },
+    )
+}
 
 @Composable
 fun ProgressBar(){
@@ -60,6 +88,15 @@ fun ProgressBar(){
         }
     }
 }
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun TopBarPreview(){
+    LifeMetricsTheme {
+        TopBar()
+    }
+}
+
 
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
