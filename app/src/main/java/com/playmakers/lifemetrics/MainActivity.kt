3e625f313a53eaf9a60c25c85edd0d8ce3f5076a
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-04 10:54:19 +0600

    Cleaning up

--- app/src/main/java/com/playmakers/lifemetrics/MainActivity.kt
+++ app/src/main/java/com/playmakers/lifemetrics/MainActivity.kt
@@ -3,20 +3,10 @@ package com.playmakers.lifemetrics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.playmakers.lifemetrics.ui.theme.LifeMetricsTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,31 +18,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ProgressBar()
+                    //
                 }
             }
         }
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun ProgressBar(){
-    Surface(
-        Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ){
-        CircularProgressIndicator(
-            progress = 1f,
-            strokeWidth = 10.dp,
-            color = Color.Gray
-        )
-
-        CircularProgressIndicator(
-            progress = 0.4f,
-            strokeWidth = 10.dp,
-        )
     }
 }
