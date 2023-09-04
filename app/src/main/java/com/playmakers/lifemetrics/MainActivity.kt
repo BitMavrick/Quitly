ec6a428b97d878cdc538445d651768115b034ca5
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-04 11:11:27 +0600

    Making the home Scaffold

--- app/src/main/java/com/playmakers/lifemetrics/MainActivity.kt
+++ app/src/main/java/com/playmakers/lifemetrics/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.playmakers.lifemetrics.ui.screens.home.HomeScreen
 import com.playmakers.lifemetrics.ui.theme.LifeMetricsTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +19,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //
+                    HomeScreen()
                 }
             }
         }
--- app/src/main/java/com/playmakers/lifemetrics/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/playmakers/lifemetrics/ui/screens/home/HomeScreen.kt
@@ -1,4 +1,42 @@
 package com.playmakers.lifemetrics.ui.screens.home
 
-class HomeScreen {
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.playmakers.lifemetrics.ui.composables.NavigationBar
+import com.playmakers.lifemetrics.ui.composables.TopBar
+import com.playmakers.lifemetrics.ui.theme.LifeMetricsTheme
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
+@Composable
+fun HomeScreen(){
+    Scaffold(
+        topBar = {
+            TopBar()
+        },
+        content = { innerPadding ->
+            Column(
+                modifier = Modifier.consumeWindowInsets(innerPadding),
+            ) {
+
+            }
+        },
+        bottomBar = {
+            NavigationBar()
+        },
+    )
+}
+
+@Preview(name = "Action Buttons Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun HomeScreenPreview(){
+    LifeMetricsTheme {
+        HomeScreen()
+    }
 }
