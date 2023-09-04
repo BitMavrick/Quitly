1be8ad4d926d075cfa92d4a951c02a8bfc30cee3
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-04 09:39:25 +0600

    Setting up the bottom navigation bar

--- app/src/main/java/com/playmakers/lifemetrics/ui/composables/HomeComposables.kt
+++ app/src/main/java/com/playmakers/lifemetrics/ui/composables/HomeComposables.kt
@@ -13,6 +13,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
@@ -33,6 +35,11 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.playmakers.lifemetrics.ui.theme.LifeMetricsTheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.remember
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -130,6 +137,22 @@ fun ActionButtons(){
     }
 }
 
+@Composable
+fun NavigationBar(){
+    NavigationBar {
+        NavigationBarItem(
+            icon = { Icon(Icons.Filled.Home, contentDescription = null)},
+            selected = true,
+            onClick = {}
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Filled.Info, contentDescription = null)},
+            selected = false,
+            onClick = {}
+        )
+    }
+}
+
 
 @Composable
 fun Quote(){
@@ -174,6 +197,14 @@ fun ActionButtonsPreview(){
 fun QuotePreview(){
     LifeMetricsTheme {
         Quote()
+    }
+}
+
+@Preview(name = "Action Buttons Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun NavigationBarPreview(){
+    LifeMetricsTheme {
+        NavigationBar()
     }
 }
 
