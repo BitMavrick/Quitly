ef6b006d623a0f959b4c65312e333d0298964c68
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-05 11:53:25 +0600

    Creating a overview card for the states screen

--- app/src/main/java/com/playmakers/lifemetrics/ui/composables/StatesComposables.kt
+++ app/src/main/java/com/playmakers/lifemetrics/ui/composables/StatesComposables.kt
@@ -1,2 +1,34 @@
 package com.playmakers.lifemetrics.ui.composables
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.playmakers.lifemetrics.ui.theme.LifeMetricsTheme
+
+@Composable
+fun OverviewCard(){
+    Card(
+        Modifier.fillMaxWidth()
+    ){
+        Column(
+            Modifier.fillMaxWidth().padding(16.dp)
+        ) {
+            Text(text = "Hello World")
+        }
+    }
+}
+
+@Preview(name = "Action Buttons Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun OverviewCardPreview(){
+    LifeMetricsTheme {
+        OverviewCard()
+    }
+}
