573e6e389c6b49edd69571e79c838cbe3966e60b
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-05 12:28:35 +0600

    Creating states screen

--- app/src/main/java/com/playmakers/lifemetrics/ui/screens/states/StatesScreen.kt
+++ app/src/main/java/com/playmakers/lifemetrics/ui/screens/states/StatesScreen.kt
@@ -1,4 +1,22 @@
 package com.playmakers.lifemetrics.ui.screens.states
 
-class StatesScreen {
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.playmakers.lifemetrics.ui.composables.OverviewCard
+import com.playmakers.lifemetrics.ui.theme.LifeMetricsTheme
+
+@Composable
+fun StatesScreen(modifier: Modifier = Modifier){
+
+}
+
+@Preview(name = "Action Buttons Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun StatesScreenPreview(){
+    LifeMetricsTheme {
+        StatesScreen()
+    }
 }
