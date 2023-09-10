20f95c4da6d53767572cebcac7ac7b68ea8f5fcc
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-10 19:43:20 +0600

    Done with the datastore

---
+++ app/src/main/java/com/playmakers/lifemetrics/LifeMetricsApplication.kt
@@ -0,0 +1,4 @@
+package com.playmakers.lifemetrics
+
+class LifeMetricsApplication {
+}
--- app/src/main/java/com/playmakers/lifemetrics/data/local/UserPreferencesRepository.kt
+++ app/src/main/java/com/playmakers/lifemetrics/data/local/UserPreferencesRepository.kt
@@ -1,8 +1,15 @@
 package com.playmakers.lifemetrics.data.local
 
+import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
 
 class UserPreferencesRepository(
     private val dataStore: DataStore<Preferences>
@@ -12,5 +19,22 @@ class UserPreferencesRepository(
         const val TAG = "UserPreferencesRepo"
     }
 
+    suspend fun saveTimePreference(theTime : String){
+        dataStore.edit { preferences ->
+            preferences[TIME_START] = theTime
+        }
+    }
 
+    val dateTime: Flow<String> = dataStore.data
+        .catch {
+            if (it is IOException) {
+                Log.e(TAG, "Error reading preferences.", it)
+                emit(emptyPreferences())
+            } else {
+                throw it
+            }
+        }
+        .map { preferences ->
+            preferences[TIME_START] ?: "-1"
+        }
 }
