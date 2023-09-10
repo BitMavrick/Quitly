ac229169b6ead9cc7323792c87a0a96243bf6aec
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-10 19:47:35 +0600

    Saving ...

--- app/src/main/java/com/playmakers/lifemetrics/LifeMetricsApplication.kt
+++ app/src/main/java/com/playmakers/lifemetrics/LifeMetricsApplication.kt
@@ -1,4 +1,22 @@
 package com.playmakers.lifemetrics
 
-class LifeMetricsApplication {
+import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.playmakers.lifemetrics.data.local.UserPreferencesRepository
+
+
+private const val DATE_TIME_NAME = "date_time"
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = DATE_TIME_NAME
+)
+class LifeMetricsApplication: Application() {
+    lateinit var userPreferencesRepository: UserPreferencesRepository
+
+    override fun onCreate() {
+        super.onCreate()
+        userPreferencesRepository = UserPreferencesRepository(dataStore)
+    }
 }
