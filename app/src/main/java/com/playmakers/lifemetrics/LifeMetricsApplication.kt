65871ce46ca8cae25d2d6abfd82eab1070e160be
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-15 16:39:38 +0600

    setting up the time preferences

--- app/src/main/java/com/playmakers/lifemetrics/LifeMetricsApplication.kt
+++ app/src/main/java/com/playmakers/lifemetrics/LifeMetricsApplication.kt
@@ -1,6 +1,23 @@
 package com.playmakers.lifemetrics
 
 import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.playmakers.lifemetrics.data.local.UserPreferencesRepository
+
+
+private const val TIME_PREFERENCE_NAME = "time_preference"
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = TIME_PREFERENCE_NAME
+)
+
 class LifeMetricsApplication : Application() {
-    //
+    lateinit var userPreferencesRepository: UserPreferencesRepository
+    override fun onCreate() {
+        super.onCreate()
+        userPreferencesRepository = UserPreferencesRepository(dataStore)
+    }
+
 }
