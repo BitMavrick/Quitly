de7dd5aac7ea562c93ad2ca5bb370504afd7e027
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-20 08:35:53 +0600

    Saving ...

---
+++ app/src/main/java/com/playmakers/lifemetrics/data/progressValue/Progress.kt
@@ -0,0 +1,7 @@
+package com.playmakers.lifemetrics.data.progressValue
+
+data class Progress(
+    val title: String,
+    val startTime: Long,
+    val endTime: Long
+)
---
+++ app/src/main/java/com/playmakers/lifemetrics/data/progressValue/ProgressDataSource.kt
@@ -0,0 +1,8 @@
+package com.playmakers.lifemetrics.data.progressValue
+
+class ProgressDataSource {
+
+}
+fun daysInSeconds(days: Int) : Long{
+    return days * 86400L // 1 day = 86,400 seconds
+}
