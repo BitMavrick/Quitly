5672d545a5a36712371610301e4e5b953e87780c
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-21 09:11:58 +0600

    Updating the animations

--- app/src/main/java/com/playmakers/lifemetrics/ui/screens/states/StatesScreen.kt
+++ app/src/main/java/com/playmakers/lifemetrics/ui/screens/states/StatesScreen.kt
@@ -1,14 +1,21 @@
 package com.playmakers.lifemetrics.ui.screens.states
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.playmakers.lifemetrics.ui.AppViewModelProvider
@@ -29,13 +36,14 @@ fun StatesScreen(
     val statesUiState by statesViewModel.statesUiState.collectAsState()
     val mainUiState by homeViewModel.uiState.collectAsState()
 
-    LazyColumn(
-        modifier = Modifier
-            .consumeWindowInsets(paddingValues)
-            .padding(16.dp),
-        contentPadding = paddingValues
-    ){
-        if(mainUiState.runningTimeSeconds != 0L){
+    if(mainUiState.runningTimeSeconds != 0L){
+        LazyColumn(
+            modifier = Modifier
+                .consumeWindowInsets(paddingValues)
+                .padding(16.dp),
+            contentPadding = paddingValues
+        ){
+
             item{
                 OverviewCard(
                     homeViewModel.statesProgressCard().title,
@@ -63,5 +71,19 @@ fun StatesScreen(
                 )
             }
         }
+    }else{
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Statistics are unavailable for now.\nPlease start the timer.",
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center
+            )
+
+        }
+
     }
 }
