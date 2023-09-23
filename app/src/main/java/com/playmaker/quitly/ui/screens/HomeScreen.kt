95febf4b69d9a83b6f5cb02d00492267730ea452
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-23 17:05:33 +0600

    Saving

--- app/src/main/java/com/playmaker/quitly/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/playmaker/quitly/ui/screens/HomeScreen.kt
@@ -1,4 +1,88 @@
 package com.playmaker.quitly.ui.screens
 
-class HomeScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.dimensionResource
+import com.playmaker.quitly.R
+import com.playmaker.quitly.data.model.ScreenType
+import com.playmaker.quitly.ui.stateModel.MainUiState
+import com.playmaker.quitly.ui.utils.ContentType
+import com.playmaker.quitly.ui.utils.NavigationType
+
+
+@Composable
+fun HomeScreen(
+    navigationType: NavigationType,
+    contentType: ContentType,
+    uiState: MainUiState,
+    onTabPressed: (ScreenType) -> Unit,
+    onDetailsScreenBackPressed: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    //
 }
+
+@Composable
+private fun BottomNavigationBar(
+    currentTab: ScreenType,
+    onTabPressed: ((ScreenType) -> Unit),
+    navigationItemContentList: List<NavigationItemContent>,
+    modifier: Modifier = Modifier
+) {
+    NavigationBar(modifier = modifier) {
+        for (navItem in navigationItemContentList) {
+            NavigationBarItem(
+                selected = currentTab == navItem.screenType,
+                onClick = { onTabPressed(navItem.screenType) },
+                icon = {
+                    Icon(
+                        imageVector = navItem.icon,
+                        contentDescription = navItem.text
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Composable
+private fun NavigationRail(
+    currentTab: ScreenType,
+    onTabPressed: ((ScreenType) -> Unit),
+    navigationItemContentList: List<NavigationItemContent>,
+    modifier: Modifier = Modifier
+) {
+    NavigationRail(modifier = modifier) {
+        for (navItem in navigationItemContentList) {
+            NavigationRailItem(
+                selected = currentTab == navItem.screenType,
+                onClick = { onTabPressed(navItem.screenType) },
+                icon = {
+                    Icon(
+                        imageVector = navItem.icon,
+                        contentDescription = navItem.text
+                    )
+                }
+            )
+        }
+    }
+}
+
+private data class NavigationItemContent(
+    val screenType: ScreenType,
+    val icon: ImageVector,
+    val text: String
+)
