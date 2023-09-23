5665520231367a3297bbb5c389428f08e4095413
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-23 17:26:53 +0600

    Done with the Navigation bar

--- app/src/main/java/com/playmaker/quitly/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/playmaker/quitly/ui/screens/HomeScreen.kt
@@ -1,22 +1,28 @@
 package com.playmaker.quitly.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.NavigationRail
 import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.playmaker.quitly.data.model.ScreenType
 import com.playmaker.quitly.ui.stateModel.MainUiState
 import com.playmaker.quitly.ui.utils.ContentType
@@ -81,7 +87,44 @@ private fun NavigationRail(
     }
 }
 
-@Preview()
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun NavigationDrawerContent(
+    selectedDestination: ScreenType,
+    onTabPressed: ((ScreenType) -> Unit),
+    navigationItemContentList: List<NavigationItemContent>,
+    modifier: Modifier = Modifier
+){
+    Column(modifier = modifier){
+        NavigationDrawerHeader(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+        )
+        for (navItem in navigationItemContentList) {
+            NavigationDrawerItem(
+                selected = selectedDestination == navItem.screenType,
+                label = {
+                    Text(
+                        text = navItem.text,
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
+                },
+                icon = {
+                    Icon(
+                        imageVector = navItem.icon,
+                        contentDescription = navItem.text
+                    )
+                },
+                colors = NavigationDrawerItemDefaults.colors(
+                    unselectedContainerColor = Color.Transparent
+                ),
+                onClick = { onTabPressed(navItem.screenType) }
+            )
+        }
+    }
+}
+
 @Composable
 private fun NavigationDrawerHeader(
     modifier: Modifier = Modifier
