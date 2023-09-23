64e711818fd639805f01b999112400ed40065ba9
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-23 17:06:07 +0600

    Optimizing ...

--- app/src/main/java/com/playmaker/quitly/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/playmaker/quitly/ui/screens/HomeScreen.kt
@@ -1,22 +1,13 @@
 package com.playmaker.quitly.ui.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationDrawerItem
-import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.NavigationRail
 import androidx.compose.material3.NavigationRailItem
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.dimensionResource
-import com.playmaker.quitly.R
 import com.playmaker.quitly.data.model.ScreenType
 import com.playmaker.quitly.ui.stateModel.MainUiState
 import com.playmaker.quitly.ui.utils.ContentType
