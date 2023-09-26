5ffab1b51deae763e003584db1f78127eed75ca0
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-09-26 12:44:41 +0600

    Done with the page setting

--- app/src/main/java/com/playmaker/quitly/ui/components/DetailComponents.kt
+++ app/src/main/java/com/playmaker/quitly/ui/components/DetailComponents.kt
@@ -10,6 +10,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.Lightbulb
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -17,9 +20,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.playmaker.quitly.ui.stateModel.MainUiState
+import com.playmaker.quitly.ui.utils.DetailType
 import com.playmaker.quitly.ui.utils.NavigationType
 
 @Composable
@@ -32,18 +36,42 @@ fun RootDetailComponents(
         onBackPressed()
     }
 
-    LazyColumn(){
+    LazyColumn {
         if(navigationType == NavigationType.BOTTOM_NAVIGATION){
             item {
+                AppDetailTopBar(
+                    uiState,
+                    onBackPressed
+                )
+            }
+        }
 
+        if(uiState.currentDetailType == DetailType.HOME_DETAIL){
+            item {
+                Column(
+                    Modifier.padding(16.dp)
+                ) {
+                    ProgressCard()
+                }
+            }
+        }else{
+            item{
+                Column(
+                    Modifier.padding(16.dp)
+                ) {
+                    AchievementCard()
+                }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-private fun AppDetailTopBar(modifier: Modifier = Modifier) {
+private fun AppDetailTopBar(
+    uiState: MainUiState,
+    onBackPressed: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
@@ -53,7 +81,7 @@ private fun AppDetailTopBar(modifier: Modifier = Modifier) {
             verticalAlignment = Alignment.CenterVertically
         ){
             IconButton(
-                onClick = { }
+                onClick = { onBackPressed() }
             ) {
                 Icon(
                     imageVector = Icons.Filled.ArrowBack,
@@ -62,11 +90,19 @@ private fun AppDetailTopBar(modifier: Modifier = Modifier) {
                 )
             }
 
-            Text(
-                text = "QUITLY",
-                style = MaterialTheme.typography.titleLarge,
-                color = MaterialTheme.colorScheme.primary,
-            )
+            if(uiState.currentDetailType == DetailType.HOME_DETAIL){
+                Text(
+                    text = "PROGRESS HISTORY",
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.primary,
+                )
+            }else{
+                Text(
+                    text = "ACHIEVEMENTS",
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.primary,
+                )
+            }
         }
 
         IconButton(
@@ -77,6 +113,99 @@ private fun AppDetailTopBar(modifier: Modifier = Modifier) {
                 contentDescription = "More option",
                 tint = MaterialTheme.colorScheme.primary
             )
+        }
+    }
+}
+
+
+@Composable
+fun ProgressCard(){
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+        ),
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Row(
+                Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(
+                    Icons.Outlined.Lightbulb,
+                    tint = MaterialTheme.colorScheme.primary,
+                    contentDescription = null,
+                )
+                Text(
+                    text = "Progress Card",
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+            }
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(
+                    text = "- This is the progress card",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun AchievementCard(){
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+        ),
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Row(
+                Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(
+                    Icons.Outlined.Lightbulb,
+                    tint = MaterialTheme.colorScheme.primary,
+                    contentDescription = null,
+                )
+                Text(
+                    text = "Achievement Card",
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+            }
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(
+                    text = "- This is the achievement card",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp),
+                    textAlign = TextAlign.Center
+                )
+            }
         }
     }
 }
