f750f08f0403777bcf3504a417a62799bd448701
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-10-17 23:01:55 +0600

    Optimized the code

--- app/src/main/java/com/playmaker/quitly/ui/components/HomeComponents.kt
+++ app/src/main/java/com/playmaker/quitly/ui/components/HomeComponents.kt
@@ -37,7 +37,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
